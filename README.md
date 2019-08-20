a245965438a648030995fd8a3e49bb27cd5d63ea
tyler-ball <tball@chef.io>
2019-08-20 14:07:58 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# policyfile_test2
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: policyfile_test2
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
